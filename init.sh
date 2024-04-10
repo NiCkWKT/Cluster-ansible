804c17c9089fabc509d9d6ec6f93d5d2b6099830
nickwkt <nickb55245@gmail.com>
2024-04-10 09:29:30 +0800

    Update

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ dnf makecache
 dnf install -y sudo systemd kmod kernel-headers kernel-tools kernel-devel kernel-debug-devel python-unversioned-command ansible-core git tmux dkms
 dnf install -y epel-release elrepo-release
 dnf group install -y "Development Tools" "System Tools" "RPM Development Tools"
-dnf install -y perl-sigtrap createrepo kernel-rpm-macros lsof tcl tk gcc-gfortran nano vim glances wget htop btop net-tools
+dnf install -y perl-sigtrap createrepo kernel-rpm-macros lsof tcl tk gcc-gfortran nano vim glances wget htop btop net-tools traceroute squashfuse iperf
