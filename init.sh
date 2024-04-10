1059ca1f35dc51a6ff745bee8a4c30b9be74ee85
nickwkt <nickb55245@gmail.com>
2024-04-10 09:28:01 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-dnf install -y sudo systemd kmod kernel-headers kernel-tools kernel-devel kernel-debug-devel python-unversioned-command ansible-core git tmux
-dnf group install -y "Development Tools"
+dnf makecache
+dnf install -y sudo systemd kmod kernel-headers kernel-tools kernel-devel kernel-debug-devel python-unversioned-command ansible-core git tmux dkms
+dnf install -y epel-release elrepo-release
+dnf group install -y "Development Tools" "System Tools" "RPM Development Tools"
+dnf install -y perl-sigtrap createrepo kernel-rpm-macros lsof tcl tk gcc-gfortran nano vim glances wget htop btop net-tools
